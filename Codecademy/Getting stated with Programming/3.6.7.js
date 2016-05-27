7d755eee186f9fd72de010fd06925f56359342c8
robin <iamufather@qq.com>
2016-05-27 17:43:34 +0800

    4.1.3传递参数

--- Codecademy/Getting stated with Programming/3.6.7.js
+++ Codecademy/Getting stated with Programming/3.6.7.js
@@ -2,16 +2,21 @@
  * @Author: robin
  * @Date:   2016-05-23 18:04:30
  * @Last Modified by:   robin
- * @Last Modified time: 2016-05-26 15:22:48
+ * @Last Modified time: 2016-05-27 17:42:59
  */
-var num = 0;
-outermost:
-    for (var i = 0; i < 10; i++) {
-        for (var j = 0; j < 10; j++) {
-            if (i == 5 && j == 5) {
-                break outermost;
-            }
-            num++;
-        }
-    }
-console.log(num);
+function addTen(num) {
+    num += 10;
+    return num;
+}
+var count = 20;
+var result = addTen(count);
+console.log(count);
+console.log(result);
+
+function setName(obj) {
+    obj.name = "Robin";
+}
+
+var person = new Object();
+setName(person);
+console.log(person.name);
