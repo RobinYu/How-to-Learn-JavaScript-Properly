a9da5e37a4e646deaa2d4e9ddb8610f906fe68b8
robin <iamufather@qq.com>
2016-05-27 18:26:39 +0800

    4.2执行环境及作用域

---
+++ Codecademy/Getting stated with Programming/4.1.4.js
@@ -0,0 +1,20 @@
+/*
+* @Author: robin
+* @Date:   2016-05-27 18:08:52
+* @Last Modified by:   robin
+* @Last Modified time: 2016-05-27 18:11:17
+*/
+
+var s="Nicholas";
+var b=true;
+var i=22;
+var u;
+var n=null;
+var o=new Object();
+
+console.log(typeof s);
+console.log(typeof i);
+console.log(typeof b);
+console.log(typeof u);
+console.log(typeof n);
+console.log(typeof o);
