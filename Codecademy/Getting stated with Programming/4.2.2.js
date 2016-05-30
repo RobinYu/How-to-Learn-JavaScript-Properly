265043ea7cfce5649bdec27e9204f8a353a0d18c
robin <iamufather@qq.com>
2016-05-30 10:00:14 +0800

    2.查询标识符 当在环境中为了读取或写入而引用一个标识符时，必须通过搜索来确定该标识符实际代表什么。搜索过程从作用域链的前端开始，向上逐级查询与给定名字匹配的标识符。如果在局部环境中找到了该标识符，搜索过程停止，变量就绪。如果在局部环境中没有找到该变量名，则继续沿作用域链向上搜索。搜索过程将一直追溯到全局环境的变量对象。如果在全局变量中也没有找到这个标识符，则意味着该变量未声明。

---
+++ Codecademy/Getting stated with Programming/4.2.2.js
@@ -0,0 +1,12 @@
+/*
+* @Author: robin
+* @Date:   2016-05-30 09:39:27
+* @Last Modified by:   robin
+* @Last Modified time: 2016-05-30 09:55:05
+*/
+var color="blue";
+function getColor() {
+return color;
+
+}
+console.log(getColor());
