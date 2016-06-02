6cb1e939ac3ad3862f385dae27b33ac122b4e63d
robin <iamufather@qq.com>
2016-06-02 14:01:43 +0800

    bith

--- Codecademy/Getting stated with Programming/4.2.js
+++ Codecademy/Getting stated with Programming/4.2.js
@@ -1,20 +1,22 @@
 /*
-* @Author: robin
-* @Date:   2016-05-27 18:27:20
-* @Last Modified by:   robin
-* @Last Modified time: 2016-05-29 14:55:51
-*/
-var color="blue";
-function changeColor(){
-	var anotherColor="red";
-	function swapColors(){
-		var tempColor=anotherColor;
-		anotherColor=color;
-		color=tempColor;
-		// 这里可以访问color和anotherCorlor和tempColor
-	}
-	// 这里可以访问color和anotherColor,但不能访问tempColor
-	swapColors();
+ * @Author: robin
+ * @Date:   2016-05-27 18:27:20
+ * @Last Modified by:   robin
+ * @Last Modified time: 2016-05-29 14:55:51
+ */
+var color = "blue";
+
+function changeColor() {
+    var anotherColor = "red";
+
+    function swapColors() {
+        var tempColor = anotherColor;
+        anotherColor = color;
+        color = tempColor;
+        // 这里可以访问color和anotherCorlor和tempColor
+    }
+    // 这里可以访问color和anotherColor,但不能访问tempColor
+    swapColors();
 }
 
 changeColor();
