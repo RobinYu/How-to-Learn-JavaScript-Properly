ef1106030d9304d01106c85c93350b3d0c288142
robin <iamufather@qq.com>
2016-06-09 17:21:01 +0800

    5.2.3栈方法

---
+++ Codecademy/Getting stated with Programming/5.2.3.js
@@ -0,0 +1,11 @@
+
+var colors = new Array();
+var count = colors.push('red', 'green');
+console.log(count);
+
+count = colors.push('black');``
+console.log(count);
+
+var item = colors.pop();
+console.log(item);
+console.log(colors.length);
