6088e53baf50fe11abd476f71574b4608c096938
robin <iamufather@qq.com>
2016-06-17 13:57:40 +0800

    闭包不能直接通过使用 this 来访问外层函数的 this 变量，因为 this 变量只有当前函数本身可以访问，而其内层函数是访问不到的 值得注意的是，许多 JavaScript 开发者喜欢把 this 存在一个叫做 that 的变量中（就像下面的代码那样）。我觉得用 that 来命名使用的时候十分不方便，所以尽量使用一个合适的名词来描述 this 所指向的对象，所以我在上述代码中使用了 var theUserObj = this。
    // 一种十分常见的写法
    var that = this;

--- Codecademy/Getting stated with Programming/内层函数维持this的值的方法.js
+++ Codecademy/Getting stated with Programming/内层函数维持this的值的方法.js
@@ -8,10 +8,10 @@ var user = {
   clickHandler: function (event) {
         // 为了当 this 还指向 user 对象的时候把它的值保存下来，我们把它存到另一个变量中
         // 我们把 this 保存到 theUserObj 变量中去，这样我们就可以在之后使用了
-    var theUserObj = this;
+    var that = this;
     this.data.forEach(function (person) {
             // 我们将 this.tournament 替换成 theUserObj.tournament
-      console.log(person.name + ' is playing at ' + theUserObj.tournament);
+      console.log(person.name + ' is playing at ' + that.tournament);
     });
   },
 };
