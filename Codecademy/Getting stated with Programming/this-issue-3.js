6f1f3d26eec8d18f7a4456f4b72df3748fa45de9
robin <iamufather@qq.com>
2016-06-17 17:50:51 +0800

    当把含有 this 的方法赋值给一个变量时维持 this 的值的方法
    我们可以使用 bind 方法来显式地设置 this 的值来解决这个问题：

--- Codecademy/Getting stated with Programming/this-issue-3.js
+++ Codecademy/Getting stated with Programming/this-issue-3.js
@@ -19,7 +19,7 @@ var user = {
 };
 
 // 把 user.showData 赋值给一个变量
-var showUserData = user.showData;
+var showUserData = user.showData.bind(user);
 
 // 当我们执行 showUserData 函数的时候，打印在 console 中的值来自于全局的 data 数组，而不是 user 对象的 data 属性
 showUserData(); // Samantha 12 （来自全局 data 数组）
