cf1fd9cae12703071b03f12b072f308db0603b58
robin <iamufather@qq.com>
2016-06-17 18:17:50 +0800

    gameController 对象借用了 appController 的 avg() 方法。在 appController.avg() 中的 this 的值会被设置成 gameController 对象，因为我们把 gameController 作为第一个参数传入了 apply() 方法中。传入 apply() 方法的第一个参数会被显式地设置为 this 的值。

--- Codecademy/Getting stated with Programming/this-issues-4.js
+++ Codecademy/Getting stated with Programming/this-issues-4.js
@@ -21,8 +21,11 @@ var appController = {
   },
 };
 
-// 如果我们执行下面的代码，
-// gameController.avgScore 属性将会被设置为 appController 对象的 scores 数组的平均数
+// 注意我们使用的是 apply() 方法，所以第二个参数必须是一个数组，这个数组中包含了要传入 appController.avg() 的参数
+appController.avg.apply(gameController, gameController.scores);
 
-// 不要执行下面这行代码，这只是用来说明的，而我们现在想让 appController.avgScore 保持 null 值
-gameController.avgScore = appController.avg();
+// 即使我们从 appController 对象中借用了 avg() 方法，gameController 的 avgScore 属性仍被成功地设置了
+console.log(gameController.avgScore); // 46.4
+
+// appController.avgScore 的值仍然是 null。它没有被更新，只有 gameController.avgScore 被更新了
+console.log(appController.avgScore); // null
